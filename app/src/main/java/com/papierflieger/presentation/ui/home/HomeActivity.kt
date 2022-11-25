43525e610e3f0c788468e44135711602d123cd30
Nawfal Hadi <nawfalhdi@gmail.com>
2022-11-26 00:59:24 +0800

    feat : slicing home fragment

--- app/src/main/java/com/papierflieger/presentation/ui/home/HomeActivity.kt
+++ app/src/main/java/com/papierflieger/presentation/ui/home/HomeActivity.kt
@@ -6,15 +6,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import com.google.android.material.shape.CornerFamily
+import com.google.android.material.shape.MaterialShapeDrawable
 import com.papierflieger.R
+import com.papierflieger.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityHomeBinding
     private lateinit var navController: NavController
 
     @SuppressLint("SourceLockedOrientationActivity")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        editBottomNavigationView()
 
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
 
@@ -23,5 +31,15 @@ class HomeActivity : AppCompatActivity() {
 
         navController = navHostFragment.navController
 
+    }
+
+    private fun editBottomNavigationView() {
+        val radius = resources.getDimension(R.dimen.radius_small)
+        val bottomNavBg = binding.bottomNavigation.background as MaterialShapeDrawable
+
+        bottomNavBg.shapeAppearanceModel = bottomNavBg.shapeAppearanceModel.toBuilder()
+            .setTopLeftCorner(CornerFamily.ROUNDED, radius)
+            .setTopRightCorner(CornerFamily.ROUNDED, radius)
+            .build()
     }
 }
