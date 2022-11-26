6eed7e8093afe29402bfdd195f06bb99fe76d0d2
Nawfal Hadi <nawfalhdi@gmail.com>
2022-11-26 19:01:33 +0800

    feat : function of the page & some button function (activated round trip)

--- app/src/main/java/com/papierflieger/presentation/ui/home/HomeFragment.kt
+++ app/src/main/java/com/papierflieger/presentation/ui/home/HomeFragment.kt
@@ -5,20 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.papierflieger.R
+import com.papierflieger.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+
+    private lateinit var binding : FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.btnSearch.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_searchFragment)
+        }
     }
 
 }
--- app/src/main/java/com/papierflieger/presentation/ui/home/search/SearchFragment.kt
+++ app/src/main/java/com/papierflieger/presentation/ui/home/search/SearchFragment.kt
@@ -5,16 +5,65 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.papierflieger.R
+import com.papierflieger.databinding.FragmentSearchBinding
 
 class SearchFragment : Fragment() {
+
+    private lateinit var binding : FragmentSearchBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
+        binding = FragmentSearchBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        allNavigation()
+        roundTripCheck()
+        switchFlightButton()
+
+        binding.switchRoundtrip.setOnClickListener {
+            roundTripCheck()
+        }
+    }
+
+    private fun switchFlightButton() {
+        with(binding){
+            btnSwitchFlight.setOnClickListener {
+                val destination  = tvDestination.text.toString()
+                val from = tvFrom.text.toString()
+
+                tvFrom.text = destination
+                tvDestination.text = from
+            }
+        }
+    }
+
+    private fun roundTripCheck() {
+        if (!binding.switchRoundtrip.isChecked) {
+            with(binding){
+                guidelineTwo.visibility = View.GONE
+                icDateBack.visibility = View.GONE
+                tvReturnDate.visibility = View.GONE
+            }
+        } else {
+            with(binding){
+                guidelineTwo.visibility = View.VISIBLE
+                icDateBack.visibility = View.VISIBLE
+                tvReturnDate.visibility = View.VISIBLE
+            }
+        }
+    }
+
+    private fun allNavigation() {
+        binding.btnBack.setOnClickListener {
+            findNavController().popBackStack()
+        }
     }
 
 }
