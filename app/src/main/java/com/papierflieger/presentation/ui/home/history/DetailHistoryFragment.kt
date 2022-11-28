61531bf993f42a509ac371158374d9562d1fb498
Nawfal Hadi <nawfalhdi@gmail.com>
2022-11-28 19:30:35 +0800

    feat : slicing detail transation

---
+++ app/src/main/java/com/papierflieger/presentation/ui/home/history/DetailHistoryFragment.kt
@@ -0,0 +1,22 @@
+package com.papierflieger.presentation.ui.home.history
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.papierflieger.R
+
+class DetailHistoryFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detail_history, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
