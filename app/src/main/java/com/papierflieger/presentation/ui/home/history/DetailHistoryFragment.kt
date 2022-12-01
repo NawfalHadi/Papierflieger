5448b2136514953b09d428db1acb8c2a045c9dda
Nawfal Hadi <nawfalhdi@gmail.com>
2022-12-01 18:57:55 +0800

    aditional : just boarding pass file

--- app/src/main/java/com/papierflieger/presentation/ui/home/history/DetailHistoryFragment.kt
+++ app/src/main/java/com/papierflieger/presentation/ui/home/history/DetailHistoryFragment.kt
@@ -6,17 +6,49 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.papierflieger.R
+import com.papierflieger.databinding.FragmentDetailHistoryBinding
 
 class DetailHistoryFragment : Fragment() {
+
+    private lateinit var binding : FragmentDetailHistoryBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+        binding = FragmentDetailHistoryBinding.inflate(layoutInflater, container, false)
         return inflater.inflate(R.layout.fragment_detail_history, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.icKeyboardArrow.setOnClickListener {
+            pricingDetailsShow()
+        }
+    }
+
+    private fun pricingDetailsShow() {
+        val arrowState = binding.icKeyboardArrow.rotation
+        with(binding){
+            if (arrowState == 180f){
+                // Hide Details
+                icKeyboardArrow.rotation = 0f
+                // guideline, rv, payment hide
+                guidelineOne.visibility = View.GONE
+                rvPricingdetail.visibility = View.GONE
+                textPayment.visibility = View.GONE
+                tvPayment.visibility = View.GONE
+
+            } else {
+                // Show Detail
+                icKeyboardArrow.rotation = 180f
+                // guideline, rv, payment show
+                guidelineOne.visibility = View.VISIBLE
+                rvPricingdetail.visibility = View.VISIBLE
+                textPayment.visibility = View.VISIBLE
+                tvPayment.visibility = View.VISIBLE
+            }
+        }
     }
 }
