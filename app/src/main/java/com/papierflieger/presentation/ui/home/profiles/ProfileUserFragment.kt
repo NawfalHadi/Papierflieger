fcf1b62da846d442c9e1b1a2bc0303b1e4a81013
Nawfal Hadi <nawfalhdi@gmail.com>
2022-12-16 21:33:04 +0800

    .

--- app/src/main/java/com/papierflieger/presentation/ui/home/profiles/ProfileUserFragment.kt
+++ app/src/main/java/com/papierflieger/presentation/ui/home/profiles/ProfileUserFragment.kt
@@ -15,7 +15,7 @@ class ProfileUserFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentProfileUserBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
